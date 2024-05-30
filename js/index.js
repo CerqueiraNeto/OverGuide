946a343685aac668146b9488ec3a7d1260070f09
Daniel <danielrodrigo098@gmail.com>
2024-05-30 19:45:18 -0300

    melhorias na chamada da api. Adicionei o arquivo api_handler.js

--- js/index.js
+++ js/index.js
@@ -1,37 +1,19 @@
+import { api_call } from "./api_handler.js";
+
 const endpoint = "https://overfast-api.tekrop.fr/heroes/ana";
 const ana_img = document.getElementById("ana_image");
 const ana_description = document.getElementById("ana_description");
 
-fetch(endpoint)
-.then(res => res.json())
-.then(hero_data => {
-
-    ana_img.src = hero_data.portrait;
-    ana_description.innerHTML = hero_data.description;
-    
-})
+api_call(endpoint, ana_img, ana_description);
 
 const endpoint2 = "https://overfast-api.tekrop.fr/heroes/ashe";
 const ashe_img = document.getElementById("ashe_image");
 const ashe_description = document.getElementById("ashe_description");
 
-// TODO a imagem da Ashe não tá carregando
-fetch(endpoint2)
-.then(res => res.json())
-.then(hero_data => {
-
-    ashe_img.src == hero_data.portrait;
-    ashe_description.innerHTML = hero_data.description;
-})
+api_call(endpoint2, ashe_img, ashe_description);
 
 const endpoint3 = "https://overfast-api.tekrop.fr/heroes/baptiste";
 const baptiste_img = document.getElementById("baptiste_image");
 const baptiste_description = document.getElementById("baptiste_description");
 
-fetch(endpoint3)
-.then(res => res.json())
-.then(hero_data => {
-
-    baptiste_img.src = hero_data.portrait;
-    baptiste_description.innerHTML = hero_data.description
-})
+api_call(endpoint3, baptiste_img, baptiste_description);
