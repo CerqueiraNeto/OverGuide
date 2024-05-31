3c96ba963a137ac5bbaa189983e2831db7478c58
Daniel <danielrodrigo098@gmail.com>
2024-05-31 14:12:51 -0300

    Adicionei mais herois e mudei o formato dos portratos

--- js/index.js
+++ js/index.js
@@ -17,3 +17,15 @@ const baptiste_img = document.getElementById("baptiste_image");
 const baptiste_description = document.getElementById("baptiste_description");
 
 api_call(endpoint3, baptiste_img, baptiste_description);
+
+const endpoint4 = "https://overfast-api.tekrop.fr/heroes/bastion";
+const bastion_img = document.getElementById("bastion_image");
+const bastion_description = document.getElementById("bastion_description");
+
+api_call(endpoint4, bastion_img, bastion_description);
+
+const endpoint5 = "https://overfast-api.tekrop.fr/heroes/brigitte";
+const brigitte_img = document.getElementById("brigitte_image");
+const brigitte_description = document.getElementById("brigitte_description");
+
+api_call(endpoint5, brigitte_img, brigitte_description);
