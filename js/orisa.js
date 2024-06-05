e6ccc8da474983403406816c1ed13c8852310a1e
unknown <cerqsneto123@gmail.com>
2024-06-05 19:54:48 -0300

    Adicionei o restante dos heróis

---
+++ js/orisa.js
@@ -0,0 +1,12 @@
+import { api_call } from "./api_handler.js";
+
+const endpoint = "https://overfast-api.tekrop.fr/heroes/orisa";
+const hero_name = document.getElementById("hero_name");
+const hero_description = document.getElementById("hero_description");
+const hero_portrait = document.getElementById("hero_portrait");
+const hero_role = document.getElementById("hero_role");
+const hero_location = document.getElementById("hero_location");
+const hero_birthday = document.getElementById("hero_birthday");
+const hero_age = document.getElementById("hero_age");
+
+api_call(endpoint, hero_name, hero_description, hero_portrait, hero_role, hero_location, hero_birthday, hero_age);
