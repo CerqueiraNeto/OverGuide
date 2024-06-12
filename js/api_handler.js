8f85ab677c55bba4b85e0ace7c94c0d81bbcbaeb
unknown <cerqsneto123@gmail.com>
2024-06-12 16:51:17 -0300

    Adicionei os hitpoints na api_handler

--- js/api_handler.js
+++ js/api_handler.js
@@ -2,11 +2,9 @@
 // Parâmetros: o endpoint da api, o elemento html do portrato, o elemento html da descrição
 // Retorno: Nada
 export function api_call(endpoint, name, description, portrait, role, location, birthday, age, hitpoints) {
- 
     fetch(endpoint)
     .then(res => res.json())
     .then(hero_data => {
-
         name.innerHTML = hero_data.name;
         description.innerHTML = hero_data.description;
         portrait.src = hero_data.portrait;
@@ -14,6 +12,13 @@ export function api_call(endpoint, name, description, portrait, role, location, 
         location.innerHTML = "Location: " + hero_data.location;
         birthday.innerHTML = "Birthday: " + hero_data.birthday;
         age.innerHTML = "Age: " + hero_data.age;
-        hitpoints.innerHTML = "Hitpoints: " + hero_data.hitpoints;
+        hitpoints.innerHTML = "Hitpoints: " +
+            `<ul>
+                <li>Armor: ${hero_data.hitpoints.armor}</li>
+                <li>Health: ${hero_data.hitpoints.health}</li>
+                <li>Shields: ${hero_data.hitpoints.shields}</li>
+                <li>Total: ${hero_data.hitpoints.total}</li>
+            </ul>`
+        ;
     })
 }
