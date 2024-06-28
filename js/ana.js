3ff4170067843ab861698f5bb279c01d8b1540fd
unknown <cerqsneto123@gmail.com>
2024-06-28 11:33:58 -0300

    Otimização de heróis adicionada

--- js/ana.js
+++ js/ana.js
@@ -1,25 +1,6 @@
-/*
-import { api_call } from "./api_handler.js";
-
-const endpoint = "https://overfast-api.tekrop.fr/heroes/ana";
-const hero_name = document.getElementById("hero_name");
-const hero_description = document.getElementById("hero_description");
-const hero_portrait = document.getElementById("hero_portrait");
-const hero_role = document.getElementById("hero_role");
-const hero_location = document.getElementById("hero_location");
-const hero_birthday = document.getElementById("hero_birthday");
-const hero_age = document.getElementById("hero_age");
-
-api_call(endpoint, hero_name, hero_description, hero_portrait, hero_role, hero_location, hero_birthday, hero_age);
-*/
-
-// Eu modifiquei a chamada da API na página da Ana para testar a implementação de novas informações
-// Arma, habilidades, etc. Depois eu vejo como padronizar isso pra todos os herois
 import { Ability, Chapter, api_callv2 } from "./api_handler.js";
 
 const endpoint = "https://overfast-api.tekrop.fr/heroes/ana";
-
-// informações básicas
 const hero_name = document.getElementById("hero_name");
 const hero_description = document.getElementById("hero_description");
 const hero_portrait = document.getElementById("hero_portrait");
@@ -28,6 +9,7 @@ const hero_location = document.getElementById("hero_location");
 const hero_birthday = document.getElementById("hero_birthday");
 const hero_age = document.getElementById("hero_age");
 const hero_hitpoints = document.getElementById("hero_hitpoints");
+
 
 //história
 
@@ -49,6 +31,7 @@ const chapter1 = new Chapter(chapter1_title, chapter1_content, chapter1_picture)
 const chapter2 = new Chapter(chapter2_title, chapter2_content, chapter2_picture);
 const chapter3 = new Chapter(chapter3_title, chapter3_content, chapter3_picture);
 const chapters = [chapter1, chapter2, chapter3];
+
 
 // habilidades
 const weapon_name = document.getElementById("weapon_name");
@@ -78,74 +61,5 @@ const ability3 = new Ability(ability3_name, ability3_desc, ability3_icon, abilit
 
 const abilities = [weapon, ability1, ability2, ability3];
 
-fetch(endpoint)
-.then(req => req.json())
-.then(data => {
-
-    // informações básicas
-    hero_name.innerHTML = data.name;
-    hero_description.innerHTML = data.description;
-    hero_portrait.src = data.portrait;
-    hero_role.innerHTML = "Role: " + data.role;
-    hero_location.innerHTML = "Location: " + data.location;
-    hero_birthday.innerHTML = "Birthday: " + data.birthday;
-    hero_age.innerHTML = "Age: " + data.age;
-    hero_hitpoints.innerHTML = "Hitpoints: " +
-            `<ul>
-                <li>Armor: ${data.hitpoints.armor}</li>
-                <li>Health: ${data.hitpoints.health}</li>
-                <li>Shields: ${data.hitpoints.shields}</li>
-                <li>Total: ${data.hitpoints.total}</li>
-            </ul>`
-    hero_summary.innerHTML = data.story.summary;
-    console.log(data.story.media.link);
-    
-    
-    
-    const chapters = data.story.chapters;
-
-    chapter1_title.innerHTML = chapters[0].title;
-    chapter1_content.innerHTML = chapters[0].content;
-    chapter1_picture.src = chapters[0].picture;
-
-    chapter2_title.innerHTML = chapters[1].title;
-    chapter2_content.innerHTML = chapters[1].content;
-    chapter2_picture.src = chapters[1].picture;
-
-    chapter3_title.innerHTML = chapters[2].title;
-    chapter3_content.innerHTML = chapters[2].content;
-    chapter3_picture.src = chapters[2].picture;
-
-
-    // habilidades
-    const abilities = data.abilities;
-
-    weapon_name.innerHTML = abilities[0].name;
-    weapon_desc.innerHTML = abilities[0].description;
-    weapon_icon.src = abilities[0].icon;
-    weapon_video.src = abilities[0].video.link.mp4;
-
-    ability1_name.innerHTML = abilities[1].name;
-    ability1_desc.innerHTML = abilities[1].description;
-    ability1_icon.src = abilities[1].icon;
-    ability1_video.src = abilities[1].video.link.mp4;
-
-    ability2_name.innerHTML = abilities[2].name;
-    ability2_desc.innerHTML = abilities[2].description;
-    ability2_icon.src = abilities[2].icon;
-    ability2_video.src = abilities[2].video.link.mp4;
-
-    ability3_name.innerHTML = abilities[3].name;
-    ability3_desc.innerHTML = abilities[3].description;
-    ability3_icon.src = abilities[3].icon;
-    ability3_video.src = abilities[3].video.link.mp4;
-
-})
-
-
-//Modelo, que possa ser usado para todos os heróis.
-//Modelo que use for (usa html em js)
-//fecth em todos os heróis
 api_callv2(endpoint, hero_name, hero_description, hero_portrait, hero_role, 
     hero_location, hero_birthday, hero_age, hero_hitpoints, hero_summary, chapters, abilities);
-
